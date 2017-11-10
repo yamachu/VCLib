f54ea35af0413eba6e2280127544e3ffc231341f
Yusuke Yamada <yamachu.dev@gmail.com>
2017-11-10 09:14:45 +0900

    無理やりスクリプト置換で対処

--- build_windows.bat
+++ build_windows.bat
@@ -1,11 +1,13 @@
 cd /d %~dp0
 if %ARCH% == x86 (
     cd src
+    move /Y wscript_x86 wscript
     python ..\tools\waf configure
     python ..\tools\waf -v
     ren build\vclib.dll x86_vclib.dll
 ) else (
     cd src
+    move /Y wscript_x64 wscript
     python ..\tools\waf configure
     python ..\tools\waf -v
     ren build\vclib.dll x64_vclib.dll
