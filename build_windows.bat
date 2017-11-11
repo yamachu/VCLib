d4964e210928c7486adef4ead7059881eb581683
Yusuke Yamada <yamachu.dev@gmail.com>
2017-11-11 19:52:31 +0900

    Double型とFloat型の両方をビルドするようにした

--- build_windows.bat
+++ build_windows.bat
@@ -1,15 +1,25 @@
 cd /d %~dp0
 if %ARCH% == x86 (
     cd src
-    move /Y wscript_x86 wscript
-    python ..\tools\waf configure
-    python ..\tools\waf -v
-    ren build\vclib.dll x86_vclib.dll
+    if %PREC% == DOUBLE (
+        python ..\tools\waf configure --msvc_version="msvc 15.0" --msvc_target="x86" --double
+        python ..\tools\waf -v
+        ren build\vclib_double.dll x86_vclib_double.dll
+    ) else (
+        python ..\tools\waf configure --msvc_version="msvc 15.0" --msvc_target="x86"
+        python ..\tools\waf -v
+        ren build\vclib_float.dll x86_vclib_float.dll
+    )
 ) else (
     cd src
-    move /Y wscript_x64 wscript
-    python ..\tools\waf configure
-    python ..\tools\waf -v
-    ren build\vclib.dll x64_vclib.dll
+    if %PREC% == DOUBLE (
+        python ..\tools\waf configure --msvc_version="msvc 15.0" --msvc_target="x86_amd64" --double
+        python ..\tools\waf -v
+        ren build\vclib_double.dll x64_vclib_double.dll
+    ) else (
+        python ..\tools\waf configure --msvc_version="msvc 15.0" --msvc_target="x86_amd64"
+        python ..\tools\waf -v
+        ren build\vclib_float.dll x64_vclib_float.dll
+    )
 )
 cd ..\
